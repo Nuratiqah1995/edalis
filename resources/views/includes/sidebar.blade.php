1df91e5d9ff57c62aa8f1c7288f2974c180d9c2b
Siti Nuratiqah Bakhtiarlili <atiqahlyly@yahoo.com>
2018-10-22 23:49:32 +0800

    New Updates

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,8 +1,8 @@
  <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="dist/img/bulb2.png" alt="EDALIS" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light" style="bold">EDALIS</span>
     </a>
 
 
